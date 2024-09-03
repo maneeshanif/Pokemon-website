cae381b484705c2287bf3b67d1e2d3b41e417aff
maneeshanif <manees7865@gmail.com>
2024-09-04 02:40:01 +0500

    intial

---
+++ pokemon-react/src/PokemonCards.jsx
@@ -0,0 +1,9 @@
+export const Pokemoncards =({pokemonData} )=>{
+ return (
+    <li className="pokemon-card">
+        <figure>
+            <img src={pokemonData.sprites.other.dream_world.front_default} alt={pokemonData.name} className="pokemon-image" />
+        </figure>
+    </li>
+ )
+}
